e65daee095444084cb9c9c992cfae384f9bfac74
goddashab <ningchenliang98@gmail.com>
2024-03-01 21:26:06 -0800

    mongodb enable

---
+++ dashboard-backend/readme.md
@@ -0,0 +1,6 @@
+use mongo compass to see the mongode interaction
+
+```sh
+mongodb://localhost:27017/dashboard
+```
+
--- dashboard-backend/services/mongoService.js
+++ dashboard-backend/services/mongoService.js
@@ -3,7 +3,7 @@
 import { MongoClient } from 'mongodb';
 import config from '../config.js';
 
-const uri = 'mongodb://localhost:27017';
+const uri = process.env.MONGO_URL|| 'mongodb://localhost:27017';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function connectToDatabase() {
